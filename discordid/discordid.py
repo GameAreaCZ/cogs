9373c704b0196e67535a282bf1831633cc32dac6
GameAreaCZ <82257025+GameAreaCZ@users.noreply.github.com>
2023-10-29 21:06:58 +0100

    Update discordid.py

--- discordid/discordid.py
+++ discordid/discordid.py
@@ -11,5 +11,5 @@ class DiscordID(commands.Cog):
         """Get the discord ID of a user."""
         if member is None:
             member = ctx.author
-        embed = discord.Embed(title=str("Tvoje DiscordID"), description=str(member.id), color=await ctx.embed_color())
+        embed = discord.Embed(title="Tvoje Discord ID", description=str(member.id), color=await ctx.embed_color())
         await ctx.send(embed=embed)
